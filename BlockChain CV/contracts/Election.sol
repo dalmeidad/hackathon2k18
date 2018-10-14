dc3f0d46eab6261ba89802eb5cfd8da970c86451
Kaixing Wu <wuk@carleton.edu>
2018-10-14 08:01:53 -0500

    BlockChain Demo uploaded

--- BlockChain CV/contracts/Election.sol
+++ BlockChain CV/contracts/Election.sol
@@ -6,8 +6,8 @@ contract Election {
         address id;
         string name;
         string country;
-        bytes32[] resume;
         uint score;
+        bytes32[] resume;
     }
     struct Company{
         address id;
@@ -24,70 +24,111 @@ contract Election {
     mapping(address => Company) public companies;
     mapping(address => HR) public HRs;
     // Store accounts that have voted
-    mapping(address => bool) public registerList;
-    mapping(address => bool) public companyList;
+    mapping(address => string) public registerList;
+    mapping(uint => Company) public companyList;
+    mapping(uint => Candidate) public candidateList;
+    mapping(uint => HR) public HRList;
     // Store Candidates
     // Fetch Candidate
     mapping(address => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
     uint public companiesCount;
+    uint public HRCount;
     // voted event
     event votedEvent (
         uint indexed _candidateId
     );
-
+    function stringsEqual(string storage _a, string memory _b) internal returns (bool) {
+        bytes storage a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if (a.length != b.length)
+            return false;
+        // @todo unroll this loop
+        for (uint i = 0; i < a.length; i ++)
+            if (a[i] != b[i])
+                return false;
+        return true;
+    }
     function Election () public {
-
+        address c1 = 0xe1e666DADF9F3D76492Fab543CD9679d665399eB;
+        initCompany(c1,'Didi','China');
+        address c2 = 0x502f0E47F460E9720AA343dCffb02A551C4de08d;
+        initCompany(c2,'Google','USA');
+        address h1 = 0x677Cc014c2aB64c4cF5BB19dfc13cBB15E078256;
+        initHR(h1,'Cathy','Google');
+        address e1 = 0xd401FEa5a5980c204885C7BA58145218977227F7;
+        bytes32[] r1;
+        initCandidate(e1,'Jon Snow', 'China');
+    }
+    function initCandidate (address a, string _name, string _country) public {
+        bytes32[] x;
+        candidates[a] = Candidate(a, _name,_country,60,x);
+        registerList[a] = 'Candidate';
+        candidatesCount++;
+        candidateList[candidatesCount] = candidates[a];
     }
 
+    function initCompany (address a,string _name, string _country) public {
+        companies[a] = Company(msg.sender, _name,_country,0);
+        registerList[a] = 'Company';
+        companiesCount++;
+        companyList[companiesCount] = companies[a];
+    }
+    function initHR (address a,string _name, string company) public {
+        HRs[a] = HR(a, _name,company);
+        registerList[a] = 'HR';
+        HRCount++;
+        HRList[HRCount] = HRs[a];
+    }
     function addCandidate (string _name, string _country) public {
         //require(!registerList[msg.sender]);
-        bytes32[] x;
-        candidates[msg.sender] = Candidate(msg.sender, _name,_country,x,0);
-        registerList[msg.sender] == true;
+        bytes32[] memory x;
+        candidates[msg.sender] = Candidate(msg.sender, _name,_country,60,x);
+        registerList[msg.sender] = "Candidate";
         candidatesCount++;
+        candidateList[candidatesCount] = candidates[msg.sender];
     }
 
     function addCompany (string _name, string _country) public {
-        require(!registerList[msg.sender]);
         companies[msg.sender] = Company(msg.sender, _name,_country,0);
-        registerList[msg.sender] == true;
-        companyList[msg.sender] == true;
+        registerList[msg.sender] = "Company";
+        companiesCount++;
+        companyList[companiesCount] = companies[msg.sender];
     }
     function addHR (string _name, string company) public {
-        require(!registerList[msg.sender]);
         HRs[msg.sender] = HR(msg.sender, _name,company);
-        registerList[msg.sender] == true;
+        registerList[msg.sender] = "HR";
+        HRCount++;
+        HRList[HRCount] = HRs[msg.sender];
     }
 
-    function request (string tasks) public {
+    function verify (uint _candidateId) public {
         // require that they haven't voted before
-        require(registerList[msg.sender]);
-        
 
-
-
-    }
-    function verify (address _candidateId,string tasks) public {
-        // require that they haven't voted before
-        require(registerList[msg.sender]);
-        require(registerList[_candidateId]);
-        require(companyList[msg.sender]);
-        bytes32 hash = keccak256(tasks);
+        require(stringsEqual(registerList[msg.sender] , 'Company'));
+        var tasks = "Jon Snow has been working in Didi for two years";
+        bytes32 hash = keccak256(abi.encodePacked(tasks));
         hashTable[hash] = true;
-        var candidate = candidates[_candidateId];
+        var candidate = candidateList[_candidateId];
         candidate.resume.push(hash);
+        require(candidate.resume.length >= 1);
 
 
 
 
-        // update candidate vote Co
     }
-    function check (address employee) public {
-        require(registerList[employee]);
-        require(registerList[msg.sender]);
-        var resume = candidates[employee].resume; 
+    function Rate (uint _companyId) public {
+        require(stringsEqual(registerList[msg.sender] , 'Candidate'));
+        var company = companyList[_companyId];
+        company.rating ++;
+
+
+
+    }
+    function check (uint employee) public {
+        require(stringsEqual(registerList[msg.sender] , "HR"));
+        var resume = candidateList[employee].resume; 
         bool checked = true;
         for (uint i=0; i<resume.length; i++) {
             checked = checked && hashTable[resume[i]];
--- BlockChain CV/src/js/app.js
+++ BlockChain CV/src/js/app.js
@@ -3,7 +3,10 @@ App = {
   contracts: {},
   account: '0x0',
   hasVoted: false,
-
+  myRole:null,
+  candidatesSelect:null,
+  candidatesCheck:null,
+  companySelect:null,
   init: function() {
     return App.initWeb3();
   },
@@ -56,56 +59,174 @@ App = {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
+    var companyContent = $("#company");
+    var HRContent = $("#HR")
+    var candidateContent = $("#candidate")
+    var add = $("#add")
     loader.show();
+    add.hide()
+    companyContent.hide();
     content.hide();
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        var myName;
+        loader.hide()
+        App.contracts.Election.deployed().then(function(instance) {
+          instance.registerList(App.account).then(function(role){
+            myRole = role
+            $("#accountAddress").html("Your Account: " + account+ " Role is "+ myRole);
+          })
+        })
       }
     });
-
+    if (App.account)
     // Load contract data
     App.contracts.Election.deployed().then(function(instance) {
       electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
-
-      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect = $('#candidatesSelect');
       candidatesSelect.empty();
-
+      candidatesCheck= $('#candidatesCheck');
+      candidatesCheck.empty();
+      companySelect = $('#companySelect');
+      companySelect.empty();
+      companiesCount = electionInstance.companiesCount();
+      HRCount = electionInstance.HRCount();
+      return electionInstance.candidatesCount();
+}).then(function(candidatesCount) {
+  
       for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
+        electionInstance.candidateList(i).then(function(candidate) {
+          var id = i-1;
           var name = candidate[1];
-          var resume = candidate[2];
-
+          var country = candidate[2];
+          console.log(i);
           // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + resume + "</td></tr>"
+          //var candidatesSelect = $('#candidatesSelect');
+          //candidatesSelect.empty();
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + country + "</td></tr>";
           candidatesResults.append(candidateTemplate);
-
-          // // Render candidate ballot option
-          // var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          // candidatesSelect.append(candidateOption);
+          console.log(candidatesSelect.size)
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>";
+          candidatesSelect.append(candidateOption);
+          candidatesCheck.append(candidateOption);
+          console.log(candidatesSelect);
         });
       }
-      return electionInstance.registerList(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
-        $('form').hide();
+      return electionInstance.companiesCount();
+}).then(function(companiesCount) {
+      for (var j = 1; j <= companiesCount; j++) {
+        // console.log(j);
+        electionInstance.companyList(j).then(function(candidate) {
+          var id = j;
+          var name = candidate[1];
+          var country = candidate[2];
+          // Render candidate Result
+          var candidateTemplate = "<tr><th> company" + id + "</th><td>" + name + "</td><td>" + country + "</td></tr>";
+          candidatesResults.append(candidateTemplate);
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>";
+          companySelect.append(candidateOption);
+        });
       }
-      loader.hide();
-      content.show();
+      return electionInstance.HRCount();
+}).then(function(HRCount) {
+        for (var k = 1; k <= HRCount; k++) {
+        electionInstance.HRList(k).then(function(candidate) {
+          var id = k;
+          var name = candidate[1];
+          var country = candidate[2];
+          // console.log(k)
+          var candidateTemplate = "<tr><th> HR" + id + "</th><td>" + name + "</td><td>" + country + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+        });
+      }
+    }).then(function(registered) {
+      if (myRole== 'Company'){
+        console.log(myRole);
+          companyContent.show();
+          candidateContent.hide()
+          HRContent.hide();
+          $("#accountAddress").html("Your Account is " + App.account+ ", Your role is "+ myRole);
+      }
+      if (myRole== 'HR'){
+        console.log(myRole);
+          companyContent.hide();
+          candidateContent.hide();
+          HRContent.show();
+          $("#accountAddress").html("Your Account is " + App.account + ", Your role is "+ myRole);
+      }
+      if (myRole== 'Candidate'){
+        console.log(myRole);
+          companyContent.hide();
+          HRContent.hide();
+          electionInstance.candidates(App.account).then(function(me){
+          $("#accountAddress").html("Hello! " + me[1]+ ", Your role is "+ myRole);
+
+          });
+      }
     }).catch(function(error) {
       console.warn(error);
     });
-  },
 
-  addCandidate: function() {
+  },
+  verify: function() {
+    var electionInstance;
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      console.log(name);
+      instance.verify(candidateId);
+    }).then(function(i) {
+      // Wait for votes to update
+      // var candidateTemplate = "<tr><th>" + App.account  + "</th><td>" + name + "</td><td>" + [] + "</td></tr>"
+      // candidatesResults.append(candidateTemplate);
+      // $("#content").hide();
+      // $("#loader").show();
+      console.log(App.account);
+      console.log(electionInstance.candidates(App.account));
+      //App.render();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+  check: function() {
+    var electionInstance;
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      console.log(name);
+      instance.check(candidateId);
+    }).then(function(i) {
+      // Wait for votes to update
+      // var candidateTemplate = "<tr><th>" + App.account  + "</th><td>" + name + "</td><td>" + [] + "</td></tr>"
+      // candidatesResults.append(candidateTemplate);
+      // $("#content").hide();
+      // $("#loader").show();
+      console.log(App.account);
+      App.render();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+    rate: function() {
+    var electionInstance;
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      instance.Rate(candidateId);
+    }).then(function(i) {
+      // Wait for votes to update
+      // var candidateTemplate = "<tr><th>" + App.account  + "</th><td>" + name + "</td><td>" + [] + "</td></tr>"
+      // candidatesResults.append(candidateTemplate);
+      // $("#content").hide();
+      // $("#loader").show();
+      //App.render();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+addCandidate: function() {
     var electionInstance;
     var name = $('#candidatesName').val();
     App.contracts.Election.deployed().then(function(instance) {
@@ -121,7 +242,7 @@ App = {
       // $("#loader").show();
       console.log(App.account);
       console.log(electionInstance.candidates(App.account));
-      App.render();
+      //App.render();
     }).catch(function(err) {
       console.error(err);
     });
